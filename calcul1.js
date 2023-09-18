a2215267a6b7fa50f69638a3bd07d3935cc52228
xavmllt <xaviermaillot100@gmail.com>
2023-09-18 16:23:28 +0200

    Début de création d'une pseudo calculatrice

---
+++ calcul1.js
@@ -0,0 +1,20 @@
+function un (a) {
+     var a = parseFloat(alert("Vous avez choisi " + a));
+    alert("L'aire du carré est de " + a*a + "cm2");
+}
+// function un() {
+//     var inputElement = document.getElementById("nombreInput");
+//     var inputValue = inputElement.value;
+    
+//     // Convertir la valeur en nombre
+//     var nombre = parseFloat(inputValue);
+    
+//     // Vérifier si la conversion a réussi
+//     if (!isNaN(nombre)) {
+//         alert("Vous avez choisi " + nombre);
+//         alert("L'aire du carré est de " + nombre * nombre + " cm2");
+//     } else {
+//         alert("Veuillez entrer un nombre valide.");
+//     }
+// }
+
